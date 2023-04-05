fde3e9eedd610d1164e6b84096db02ddf2837f7b
Cookie <yumzuko@gmail.com>
2023-04-05 11:26:12 +0200

    Code en +

--- Projet.c
+++ Projet.c
@@ -1,205 +1,217 @@
-// Include necessary libraries
+#include <SDL2/SDL.h>
 #include <stdio.h>
-#include <Windows.h>
-// #include <SDL2/SDL.h>
+#include <stdlib.h>
 
 // Define constants for grid size
-#define max_y 10
-#define max_x 50
-#define true 1
-const int WIDTH = 800, HEIGHT = 600;
+#define MAX_Y 100
+#define MAX_X 100
 
-// int main(int argc, char *argv[]) {
-//     // Initialize SDL
-//     SDL_Init( SDL_INIT_EVERYTHING);
-//     SDL_Window *window = SDL_CreateWindow( "Jeu de la vie", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI); 
-//     if (window == NULL) {
-//         // std::cout << "Window could not be created! SDL Error: " << SDL_GetError();
-//         return 1;
-//     }
-//     SDL_Event windowEvent;
+// Global variables
+const int WIDTH = 800;
+const int HEIGHT = 800;
+const int CELL_W = WIDTH / MAX_X;
+const int CELL_H = HEIGHT / MAX_Y;
 
-//     while (true) {
-//         if (SDL_PollEvent(&windowEvent)) {
-//             if (windowEvent.type == SDL_QUIT)
-//             {break;}
-//         }
-//     }
+// Function prototypes
+void InitSetup();
+int GrilleDebut(int grille[MAX_Y][MAX_X]);
+void AfficherGrille(int grille[MAX_Y][MAX_X]);
+void Count(int grille[MAX_Y][MAX_X]);
+int Mort(int grille[MAX_Y][MAX_X]);
 
-//     SDL_DestroyWindow(window);
-//     SDL_Quit();
+// SDL variables
+SDL_Window* screen = NULL;
+SDL_Renderer* renderer = NULL;
+SDL_Event event;
 
-//     return EXIT_SUCCESS;
-// }
+void InitSetup() {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, SDL_WINDOW_SHOWN, &screen, &renderer);
+    SDL_SetWindowTitle(screen, "Game of Life");
+}
 
-int GrilleDebut(int grille[100][100],int cellule_vivante,int cellule_mort) {
+int GrilleDebut(int grille[][MAX_X]) {
     int rep11 = 0;
     int rep22 = 0;
-    int cases = 0;
+    int cases = 0;    
     int ok = 0;
-    for (int a = 0; a < max_y; a++) {
-        for (int b = 0; b < max_x; b++) {
-            grille[a] [b] = cellule_mort;
+    for (int a = 0; a < MAX_Y; a++) {
+        for (int b = 0; b < MAX_X; b++) {
+            grille[a][b] = 0;
         }
     }
     while (ok == 0) {
-    printf("Combien voulez vous mettre de cases ? : ");
-    scanf("%d", &cases);
-    // Loop through each cell and prompt user for which ones should be alive
-    for (int e = 0; e < cases; e++) {
+        printf("Combien voulez vous mettre de cases ? : ");
+        scanf("%d", &cases);
         fflush(stdin);
-        printf("\n  Choissisez une case en Y (%d a %d): ",1,max_y);
-        scanf("%d",&rep11);
-        printf("  Choissisez une case en X (%d a %d): ",1,max_x);
-        scanf("%d",&rep22);
-        // Set the specified cell to be alive
-        for (int f = 0; f < max_y; f++) {
-            for (int g = 0; g < max_x; g++) {
-                if (f == rep11-1 && g == rep22-1) {
-                    grille[f][g] = cellule_vivante;
-                }
-            }
+        // Loop through each cell and prompt user for which ones should be alive
+        for (int e = 0; e < cases; e++) {
+            printf("\n  Choissisez une case en Y (%d a %d): ", 1, MAX_Y);
+            scanf("%d", &rep11);
+            printf("  Choissisez une case en X (%d a %d): ", 1, MAX_X);
+            scanf("%d", &rep22);
+            // Set the specified cell to be alive
+            grille[rep11 - 1][rep22 - 1] = 1;
         }
-    }
-    ok++;
-    }
-    return grille;
-}
-
-int AfficherGrille (int x[100][100]) {
-    int i, j;
-    for (i = 0; i < max_y; i++) {
-        for (j = 0; j < max_x; j++) {
-            printf("%d ", x[i][j]);
-        }
-        printf("\n");
+        ok++;
     }
     return 0;
 }
-int Count(int grille[100][100],int cellule_vivante,int cellule_mort) {
-    int temp[100][100] = {0};
-    for (int j = 0; j < max_y; j++) {
-        for (int k = 0; k < max_x; k++) {
-            temp[j][k] = grille[j][k];
+
+void AfficherGrille(int grille[][MAX_X]) {
+    for (int i = 0; i < MAX_Y; i++) {
+        for (int j = 0; j < MAX_X; j++) {
+            printf("%d ", grille[i][j]);
         }
+        printf("\n");
     }
-    for (int l = 0; l < max_y; l++) {
-        for (int m = 0; m < max_x; m++) {
+}
+
+void Count(int grille[][MAX_X]) {
+    int temp[MAX_Y][MAX_X] = { 0 };
+    for (int l = 0; l < MAX_Y; l++) {
+        for (int m = 0; m < MAX_X; m++) {
             int count1 = 0;
             int count2 = 0;
-            // Count number of alive cells around the current cell
-            if (grille[l][m] == cellule_vivante) {
-                if (grille[l+1][m-1] == cellule_vivante) {
-                    count1++;    
-                }
-                if (grille[l+1][m] == cellule_vivante) {
+
+            if (grille[l][m] == 1) {
+                if (grille[l + 1][m - 1] == 1) {
                     count1++;
                 }
-                if (grille[l+1][m+1] == cellule_vivante) {
+                if (grille[l + 1][m] == 1) {
                     count1++;
                 }
-                if (grille[l][m-1] == cellule_vivante) {
+                if (grille[l + 1][m + 1] == 1) {
                     count1++;
                 }
-                if (grille[l][m+1] == cellule_vivante) {
+                if (grille[l][m - 1] == 1) {
                     count1++;
                 }
-                if (grille[l-1][m+1] == cellule_vivante) {
+                if (grille[l][m + 1] == 1) {
                     count1++;
                 }
-                if (grille[l-1][m-1] == cellule_vivante) {
+                if (grille[l - 1][m + 1] == 1) {
                     count1++;
                 }
-                if (grille[l-1][m] == cellule_vivante) {
+                if (grille[l - 1][m - 1] == 1) {
+                    count1++;
+                }
+                if (grille[l - 1][m] == 1) {
                     count1++;
                 }
                 // Update value of cell based on its surroundings
-                if (count1 > 1 && count1 < 4 ) {
-                    temp[l][m] = cellule_vivante;
+                if (count1 > 1 && count1 < 4) {
+                    temp[l][m] = 1;
                 }
                 if (count1 < 2 || count1 > 3) {
-                    temp[l][m] = cellule_mort;
+                    temp[l][m] = 0;
                 }
             }
 
             // Count number of alive cells around the current dead cell and update it accordingly
-            if (grille[l][m] == cellule_mort) {
-                if (grille[l+1][m-1] == cellule_vivante) {
-                    count2++;    
-                }
-                if (grille[l+1][m] == cellule_vivante) {
+            if (grille[l][m] == 0) {
+                if (grille[l + 1][m - 1] == 1) {
                     count2++;
                 }
-                if (grille[l+1][m+1] == cellule_vivante) {
+                if (grille[l + 1][m] == 1) {
                     count2++;
                 }
-                if (grille[l][m-1] == cellule_vivante) {
+                if (grille[l + 1][m + 1] == 1) {
                     count2++;
                 }
-                if (grille[l][m+1] == cellule_vivante) {
+                if (grille[l][m - 1] == 1) {
                     count2++;
                 }
-                if (grille[l-1][m+1] == cellule_vivante) {
+                if (grille[l][m + 1] == 1) {
                     count2++;
                 }
-                if (grille[l-1][m-1] == cellule_vivante) {
+                if (grille[l - 1][m + 1] == 1) {
                     count2++;
                 }
-                if (grille[l-1][m] == cellule_vivante) {
+                if (grille[l - 1][m - 1] == 1) {
+                    count2++;
+                }
+                if (grille[l - 1][m] == 1) {
                     count2++;
                 }
                 if (count2 > 2 && count2 < 4) {
-                    temp[l][m] = cellule_vivante;
+                    temp[l][m] = 1;
                 }
             }
         }
     }
-    for (int n = 0; n < max_y; n++) {
-        for (int o = 0; o < max_x; o++) {
+    for (int n = 0; n < MAX_Y; n++) {
+        for (int o = 0; o < MAX_X; o++) {
             grille[n][o] = temp[n][o];
         }
     }
-    return grille;
 }
 
-int Mort(int grille[100][100],int cellule_mort,int mort) {
-    int count_die=0;
-    for (int i = 0; i < max_y; i++) {
-        for (int j = 0; j < max_x; j++) {
-            if (grille [i][j] == cellule_mort) {
+int Mort(int grille[][MAX_X]) {
+    int count_die = 0;
+    for (int i = 0; i < MAX_Y; i++) {
+        for (int j = 0; j < MAX_X; j++) {
+            if (grille[i][j] == 0) {
                 count_die++;
             }
         }
     }
-    if (count_die == max_x*max_y) {
+    if (count_die == MAX_X * MAX_Y) {
         printf("C'est finit !");
-        mort = 1;
-    } 
-    return mort;
+        return 1;    }
+    return 0;
 }
 
-int main() {
-
-    int cellule_vivante = 1;
-    int cellule_mort = 0;
+int main(int argc, char *argv[]) {
+    // Initialize SDL
+    InitSetup();
+    SDL_Event windowEvent;
 
     // Create a 2D array to hold the grid
-    int grille[100][100] = {0};
+    int grille[MAX_Y][MAX_X] = { 0 };
 
-    printf("\n");
-    
-    AfficherGrille(grille);
-    GrilleDebut(grille, cellule_vivante, cellule_mort);
+    // Prompt user to input cell positions
+    printf("Choose the initial positions of the cells\n");
+    GrilleDebut(grille);
 
     int mort = 0;
     while (mort == 0) {
-        AfficherGrille(grille);
-        printf("\n");
-        Count(grille,cellule_vivante,cellule_mort);
-        mort = Mort(grille,cellule_mort,mort);
-        
+        // Clear the screen
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // black
+        SDL_RenderClear(renderer);
+
+        // Draw the cells
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // white
+        for (int i = 0; i < MAX_Y; i++) {
+            for (int j = 0; j < MAX_X; j++) {
+                if (grille[i][j] == 1) {
+                    SDL_Rect destination = { j * CELL_W, i * CELL_H, CELL_W, CELL_H };
+                    SDL_RenderFillRect(renderer, &destination);
+                }
+            }
+        }
+
+        // Update the screen
+        SDL_RenderPresent(renderer);
+
+        // Calculate the next generation of cells
+        Count(grille);
+
         // Pause for one second between each generation
-        Sleep(500);
+        SDL_Delay(100);
+
+        // Check for quit event
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                mort = 1;
+            }
+        }
     }
+
+    // Quit SDL
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(screen);
+    SDL_Quit();
+
     return 0;
-} 
+}
